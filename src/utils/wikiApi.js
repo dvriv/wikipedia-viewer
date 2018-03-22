435b1f98f81001990f1ad7fcac808ed5b27e4ae7
David <djro1605@gmail.com>
2018-03-22 03:08:24 -0600

    try catch on wiki api functions

--- src/utils/wikiApi.js
+++ src/utils/wikiApi.js
@@ -2,20 +2,23 @@ import fetch from 'node-fetch';
 
 const wikiSearch = async (searchTerm, offset = 0) => {
   if (searchTerm) {
-    const apiURL = `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=${searchTerm}&gsrlimit=20&prop=extracts&exintro&explaintext&exsentences=2&exlimit=max&gsroffset=${offset}`;
-    const response = await fetch(apiURL, {
-      headers: {
-        'Api-User-Agent': 'wiki-viewer/1.0',
-        'content-type': 'application/json',
-      },
-    });
+    try {
+      const apiURL = `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=${searchTerm}&gsrlimit=20&prop=extracts&exintro&explaintext&exsentences=2&exlimit=max&gsroffset=${offset}`;
+      const response = await fetch(apiURL, {
+        headers: {
+          'Api-User-Agent': 'wiki-viewer/1.0',
+          'content-type': 'application/json',
+        },
+      });
 
-    const data = await response.json();
-    console.log(data);
-    return data;
+      const data = await response.json();
+      return data;
+    }
+    catch (error) {
+      throw Error('Error when getting a response from the Wikipedia API');
+    }
   }
-  console.log('No Search term specified');
-  return new Error('No search term specified');
+  throw Error('No search term specified');
 };
 
 export default wikiSearch;
