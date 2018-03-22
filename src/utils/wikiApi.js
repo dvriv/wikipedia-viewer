fd29531b4dbeb27bed7b8b3f64ebb840ab7c87cf
David <djro1605@gmail.com>
2018-03-22 00:49:43 -0600

    change from 10 to 20 results

--- src/utils/wikiApi.js
+++ src/utils/wikiApi.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 const wikiSearch = async (searchTerm, offset = 0) => {
   if (searchTerm) {
-    const apiURL = `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=${searchTerm}&gsrlimit=10&prop=extracts&exintro&explaintext&exsentences=2&exlimit=max&gsroffset=${offset}`
+    const apiURL = `https://en.wikipedia.org/w/api.php?origin=*&format=json&action=query&generator=search&gsrnamespace=0&gsrsearch=${searchTerm}&gsrlimit=20&prop=extracts&exintro&explaintext&exsentences=2&exlimit=max&gsroffset=${offset}`;
     const response = await fetch(apiURL, {
       headers: {
         'Api-User-Agent': 'wiki-viewer/1.0',
